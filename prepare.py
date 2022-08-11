be0b5a2a26bc79e71db1d92e2ac81a7d68200825
JulesMorris <julessa.morris@gmail.com>
2022-08-11 13:50:43 -0500

    edits

--- prepare.py
+++ prepare.py
@@ -289,6 +289,8 @@ def wrangle_fifa_data(df):
     df.year_joined = df.year_joined.astype(int)
     #add seniority column
     df['seniority'] = df.year - df.year_joined
+    #add bmi column df_20['weight_kg'] / (df_20['height_cm']/100)**2
+    df['bmi'] = df['weight_kg'] / (df['height_cm']/100) ** 2
    
     return df
 
